14ca6900f4b4e6ccdd9c3ae7cab314653c2b15f3
kavtelishvilidemetre-creator <kavtelishvilidemetre@gmail.com>
2026-02-26 18:19:30 +0400

    support

---
+++ portholio-progect/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './header/header.jsx'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <Header />
+      <div>i am beginer but i tried maximum </div>
+    </>
+  )
+}
+
+export default App
---
+++ portholio-progect/src/header/header.jsx
@@ -0,0 +1,17 @@
+import "./header.css";
+
+function Header(){
+    return(
+        <>
+            <ul>
+                <li><a href="#">home</a></li>
+                <li><a href="#">about</a></li>
+                <li><a href="#">services</a></li>
+                <li><a href="#">portfolio</a></li>
+                <li><a href="#">contact</a></li>
+            </ul>
+        </>
+    );
+}
+
+export default Header;
