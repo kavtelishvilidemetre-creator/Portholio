1577fa13da78587be71a3a68bde21d6025317e86
kavtelishvilidemetre-creator <kavtelishvilidemetre@gmail.com>
2026-03-17 17:27:31 +0400

    few new

--- portholio-progect/src/skills/skills.jsx
+++ portholio-progect/src/skills/skills.jsx
@@ -1,17 +1,27 @@
 import './skills.css'
 import { useEffect, useState } from 'react'
+import data from './Data.json'
 
 function Skills(){
-    //const [skills, setSkills] = useState([])
-    //const url = 'https://raw.githubusercontent.com/kavtelishvilidemetre-creator/Portholio/refs/heads/main/portholio-progect/src/skills/Data.json'
-    //useEffect(() => {
-    //    fetch(url)
-    //    .then(res => res.json())
-    //    .then(console.log(res))
-    //});
+    const [skills, setSkills] = useState([])
+    const url = 'https://raw.githubusercontent.com/kavtelishvilidemetre-creator/Portholio/refs/heads/main/portholio-progect/src/skills/Data.json'
+    useEffect(() => {
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setSkills(data))
+    }, []);
     return(
         <div className="skills">
-            skills
+            {skills.map((skills)=>{
+                return (
+                <div key={skills.id}>{skills.name}
+                    <img src={skills.image} alt={skills.name} />
+                    
+                    
+                </div>
+
+                )
+            })}
         </div>
     )
 }
