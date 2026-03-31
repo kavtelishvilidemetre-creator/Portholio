9e6f103a4f0bfa5098d9bf909954b23a240c0e06
kavtelishvilidemetre-creator <kavtelishvilidemetre@gmail.com>
2026-03-31 18:39:16 +0400

    i tried everithing

--- portholio-progect/src/skills/skills.jsx
+++ portholio-progect/src/skills/skills.jsx
@@ -1,33 +1,36 @@
 import './skills.css'
-import { useEffect, useState } from 'react'
+import { use, useEffect, useState } from 'react'
 import data from './Data.json'
 
 function Skills(){
     const [skills, setSkills] = useState([])
+    const [loading, setLoading] = useState(true)
     const url = 'https://raw.githubusercontent.com/kavtelishvilidemetre-creator/Portholio/refs/heads/main/portholio-progect/src/skills/Data.json'
     useEffect(() => {
+        setLoading(true);
         fetch(url)
         .then(res => res.json())
     .then(data => {
         setSkills(data)
         console.log(data)
     })
+    .then(data => {
+      setSkills(data);
+      setLoading(false);
+    });
     }, [])
     
 
     return(
         <>
-            
+        <h1 className={{loading} ? 'loading' : 'loaded'}>loading...</h1>
            <div className="skills">
-               {
-               
-                   skills.map((skill)=>(
-                       <div key={skill.id}>
-                           {skill.name}
-                           <img src={skill.img} alt={skill.name} onClick={() => console.log(skill.name)}/>
-                       </div>
-                   ))
-                }
+                     {skills.map((skill) => (
+        <div key={skill.id}>
+          <p>{skill.name}</p>
+          <img src={skill.img} alt={skill.name} />
+        </div>
+      ))}
            </div>
         </>
     )
